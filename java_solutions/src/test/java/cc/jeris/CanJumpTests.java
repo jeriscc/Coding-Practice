293fd235f74543d36bb77a84ebdfe8efc3a2ead0
jeriscc <jeriscc@users.noreply.github.com>
2019-08-31 16:43:03 -0700

    added test case for can_jump_two

--- java_solutions/src/test/java/cc/jeris/CanJumpTests.java
+++ java_solutions/src/test/java/cc/jeris/CanJumpTests.java
@@ -13,7 +13,7 @@ public class CanJumpTests {
     @Test
     public void basicTest() {
         SlowBFSSolution s = new SlowBFSSolution();
-        int[] nums = {2,3,1,1,4};
+        int[] nums = { 2, 3, 1, 1, 4 };
         assertTrue("Slow BFS should work", s.jump(nums) == 2);
 
         BFSSolution b = new BFSSolution();
@@ -23,10 +23,13 @@ public class CanJumpTests {
         assertTrue("Greedy should work", g.jump(nums) == 2);
     }
 
+    /**
+     * Tests where the end is unreachable.
+     */
     @Test
     public void testImpossible() {
+        int[] nums = { 2, 0, 0, 1, 4 };
         SlowBFSSolution s = new SlowBFSSolution();
-        int[] nums = {2,0,0,1,4};
         assertTrue("Slow BFS should work", s.jump(nums) == -1);
 
         BFSSolution b = new BFSSolution();
@@ -34,5 +37,32 @@ public class CanJumpTests {
 
         GreedySolution g = new GreedySolution();
         assertTrue("Greedy should work", g.jump(nums) == -1);
+    }
+
+    /**
+     * Tests solutions against a very large array. Not testing on the slow solution
+     * because it will be too slow.
+     */
+    @Test
+    public void testLong() {
+        int largeSize = 10000;
+        int[] nums = new int[largeSize];
+        /**
+         * Fill the first n - 2 values with n - 2 to 1. None of them will reach the end.
+         */
+        for (int i = 0; i < largeSize - 2; i++) {
+            nums[i] = largeSize - 2 - i;
+        }
+        /**
+         * Make the second to last element (reachable by all of the previous n-2
+         * elements) able to reach the end.
+         */
+        nums[largeSize - 2] = 1;
+
+        BFSSolution b = new BFSSolution();
+        assertTrue("BFS should work", b.jump(nums) == 2);
+
+        GreedySolution g = new GreedySolution();
+        assertTrue("Greedy should work", g.jump(nums) == 2);
     }
 }
