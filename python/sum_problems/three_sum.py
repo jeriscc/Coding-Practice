16093222c8c385b83d335f451c24b9a5255b49eb
jeriscc <jeriscc@users.noreply.github.com>
2019-10-05 18:30:04 -0700

    udpated understanding of three_sum

--- python/sum_problems/three_sum.py
+++ python/sum_problems/three_sum.py
@@ -8,19 +8,35 @@ Source: https://leetcode.com/problems/3sum/
 class Solution:
     """
     Time: 28ms and 100%
-    Before was 704ms and 90%. The key to the fast speed is computing the end of the outer loop
+    Before was 704ms and 90%. The reason of the fast speed is computing the end of the outer loop
     using the the `next` function and the terminating if index == 0. Rather than breaking in the
     outer loop when we reach the first positive value.
-    """
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        result = []
 
-        index = next(x for x,val in enumerate(nums) if val >0)
-        if index == 0 or len(nums) < 3: return result
+    Fidelled with code more. Seems like the reason why it was so fast has to do with the `next` function
+    throwing the StopIteration error when it can't find a positive number. When we catch the StopIteration
+    or prevent it from ever happening, the running time shoots up too around 700.
+
+    Decided to handle the StopIteration Error and now the runtime is 716ms and 88%.
+    """
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if not nums or len(nums) < 3:
+            return []
+        nums = sorted(nums)
+
+        if nums[0] > 0 or nums[-1] < 0:
+            return []
+        if nums[0] == 0: return [[0,0,0]] if nums[2] == 0 else []
+        # if the next line is commented out, or if we don't handle the case when the last
+        # is 0 but no solution exists, the runtime goes down to around 30ms
+        if nums[-1] == 0: return [[0,0,0]] if nums[-3] == 0 else []
+
+        index = next(x for x, val in enumerate(nums) if val > 0)
+        result = []
         for i in range(index):
             # we skip visited beginnings
-            if i > 0 and nums[i] == nums[i-1]: continue
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             # perform two sum sweep
             lo, hi = i + 1, len(nums) - 1
             while lo < hi:
@@ -44,6 +60,7 @@ class Solution:
 def tests():
     s = Solution()
     assert s.threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+    assert s.threeSum([0, 0, 0, 0]) == [[0, 0, 0]]
 
 
 tests()
